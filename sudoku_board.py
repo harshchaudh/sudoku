44df8be9c29627e2daecdb5788414e15ac7bbe7f
Harsh Chaudhari <92026694+harshchaudh@users.noreply.github.com>
2023-01-03 01:47:48 +0800

    Add files via upload

---
+++ sudoku_board.py
@@ -0,0 +1,20 @@
+# 2-D array
+size = 9
+board = [[0 for i in range(size)] for j in range(size)]
+
+# grid assignment
+grid = 1 # no. corresponds to grid in sudoku_puzzles.txt
+puzzle = open("sudoku_puzzles.txt", "r")
+for line in puzzle:
+    print(line)
+
+
+
+
+
+
+
+
+
+for row in board:
+    print(row)
